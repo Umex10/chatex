7647d7186f03f5b52f181c9ea42672b991298a18
Umejr Dzinovic <Umejr.Dzinovic@edu.fh-joanneum.at>
2026-02-09 10:54:32 +0100

    git init

---
+++ backend/src/test/java/org/devtiro/chatex/ChatexApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devtiro.chatex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
